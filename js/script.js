9bc83130d07067f396b5114a52b563e34c8f7d07
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2023-08-04 21:45:46 +0900

    描画方法改良

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let orgImgData;
-let tryImgData;
-let curImgData;
+let orgImgData
+let tryImgData
+let curImgData
 
 const orgImgElm = document.createElement("img")
 
@@ -8,8 +8,16 @@ const orgCanvasElm = document.querySelector("canvas:nth-of-type(1)")
 const tryCanvasElm = document.querySelector("canvas:nth-of-type(2)")
 const curCanvasElm = document.querySelector("canvas:nth-of-type(3)")
 const curSvgElm = document.querySelector("svg")
+const gElm = document.createElementNS("http://www.w3.org/2000/svg", "g")
+const styleElm = document.createElementNS("http://www.w3.org/2000/svg", "style")
+styleElm.textContent = `text {
+    font-family: Courier;
+    text-anchor: middle;
+    dominant-baseline: central;
+}`
+curSvgElm.appendChild(styleElm)
+curSvgElm.appendChild(gElm)
 
-orgCanvasElm.style.display = "none"
 curCanvasElm.style.display = "none"
 tryCanvasElm.style.display = "none"
 
@@ -19,9 +27,9 @@ const curCtx = curCanvasElm.getContext("2d", { willReadFrequently: true })
 
 const txtElm = document.querySelector("div")
 
-charArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const charArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?"
 
-orgImgElm.onload = async evt => {
+orgImgElm.onload = _evt => {
     const { width, height } = orgImgElm
 
     orgCanvasElm.width = curCanvasElm.width = tryCanvasElm.width = curSvgElm.width = width
@@ -35,6 +43,9 @@ orgImgElm.onload = async evt => {
     tryCtx.rect(0, 0, width, height)
     tryCtx.fill()
 
+    tryCtx.textAlign = "center"
+    tryCtx.textBaseline = "middle"
+
     let cnt = 0
     let drawCnt = 0
     let tryTotal = 0
@@ -46,31 +57,25 @@ orgImgElm.onload = async evt => {
             `draw: ${drawCnt}<br>` +
             `difference: ${curTotal}`
 
-        tryImgData = tryCtx.getImageData(0, 0, width, height)
-        curImgData = curCtx.getImageData(0, 0, width, height)
-
         const x = Math.floor(Math.random() * width)
-        const y = Math.floor(Math.random() * height)
-        const size = Math.floor(Math.random() * height)
+        const y = Math.floor(Math.random() * width)
+        const size = Math.ceil(Math.random() * width)
         const char = charArr[Math.floor(Math.random() * charArr.length)]
-        rgbArr = [
-            Math.floor(Math.random() * 256),
-            Math.floor(Math.random() * 256),
-            Math.floor(Math.random() * 256),
-        ]
+        const hue = Math.floor(360 * Math.random())
+        const saturation = Math.ceil(Math.random() * 98)
+        const color = `hsl(${hue} 100% ${saturation}%)`
 
-        tryCtx.fillStyle = `rgb(${rgbArr.join(", ")})`
+        tryCtx.fillStyle = color
         tryCtx.font = `${size}px Courier`
-        tryCtx.textAlign = "center"
-        tryCtx.textBaseline = "middle"
         tryCtx.fillText(char, x, y)
 
         cnt++
 
         tryImgData = tryCtx.getImageData(0, 0, width, height)
+        curImgData = curCtx.getImageData(0, 0, width, height)
 
-        tryTotal = [...orgImgData.data].reduce((p, c, i) => p + Math.abs(c - tryImgData.data[i]), 0)
-        curTotal = [...orgImgData.data].reduce((p, c, i) => p + Math.abs(c - curImgData.data[i]), 0)
+        tryTotal = [...orgImgData.data].reduce((p, c, i) => p + (c - tryImgData.data[i]) ** 2, 0)
+        curTotal = [...orgImgData.data].reduce((p, c, i) => p + (c - curImgData.data[i]) ** 2, 0)
 
         if (tryTotal < curTotal) {
             curCtx.putImageData(tryImgData, 0, 0)
@@ -80,12 +85,9 @@ orgImgElm.onload = async evt => {
             textElm.setAttribute("x", x)
             textElm.setAttribute("y", y)
             textElm.setAttribute("font-size", size)
-            textElm.setAttribute("font-family", "Courier")
-            textElm.setAttribute("text-anchor", "middle")
-            textElm.setAttribute("dominant-baseline", "central")
-            textElm.setAttribute("fill", tryCtx.fillStyle)
+            textElm.setAttribute("fill", color)
             textElm.textContent = char
-            curSvgElm.appendChild(textElm)
+            gElm.appendChild(textElm)
         } else {
             tryCtx.putImageData(curImgData, 0, 0)
         }
